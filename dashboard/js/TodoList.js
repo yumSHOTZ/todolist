57beb7c0d053cf32b4a3a07b821855aa1db99315
michael.tablante <mtablate@venn.bio>
2018-10-02 16:58:25 +0800

    addedd few lines for creating element inside an element on event listener

--- dashboard/js/TodoList.js
+++ dashboard/js/TodoList.js
@@ -3,19 +3,28 @@ var addtextbox = document.querySelector("#addtodo");
 var todoitem1 = document.querySelector("#todoitem1");
 var left = document.querySelector("#left");
 var todoitem6 = document.querySelector("#todoitem6");
+var addbtn = document.querySelector("#addbtn");
+
+addbtn.addEventListener("click", function(){
+	 todoitem1.setAttribute("value",add.value)
+		var node = document.createElement("div");
+    	var textnode = document.createTextNode("asdf");
+    	node.appendChild(t);
+   		document.querySelector("#leftz").appendChild(node);
+
+});
+
 
 addtextbox.addEventListener("keypress", function(e){
 	var key = e.which || e.keyCode;
     if (key === 13) {     
-	todoitem1.setAttribute("value",add.value)
-	todoitem6.classList.toggle("todoitem6");
-	;
+	var addline = document.createElement("div");
+    var t = document.createTextNode("asdf");
+    addline.appendChild(t);
+    document.querySelector("todolist1").appendChild(addline);
 }
+	;
 });
-
-
-
-
 
 
 // todo -     	ADD a toggle function that disables and enables an <input type text> inside #left
@@ -23,10 +32,11 @@ addtextbox.addEventListener("keypress", function(e){
 //				[addtextbox] when [enter key] is pressed
 
 
+// SAMPLE CODE OF CREATE EVENT
+// function addItem() {
+//     var addline = document.createElement("div");
+//     var t = document.createTextNode("asdf");
+//     addline.appendChild(t);
+//     document.body.appendChild(addline);
+// }
 
-function addItem() {
-    var btn = document.createElement("div");
-    var t = document.createTextNode("asdf");
-    btn.appendChild(t);
-    document.body.appendChild(btn);
-}
