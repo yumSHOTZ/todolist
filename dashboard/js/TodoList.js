9f4d66a0e02a80818160e1b17a3667bb5afb0e71
michael.tablante <mtablate@venn.bio>
2018-10-03 15:17:48 +0800

    added functionality of add item and updated css file for style of dashboard

--- dashboard/js/TodoList.js
+++ dashboard/js/TodoList.js
@@ -1,20 +1,37 @@
-var add = document.querySelector("#addtodo");
 var addtextbox = document.querySelector("#addtodo");
-var todoitem1 = document.querySelector("#todoitem1");
 var left = document.querySelector("#left");
-var todoitem6 = document.querySelector("#todoitem6");
+var addbtn = document.querySelector("#addbtn");
 
-addtextbox.addEventListener("keypress", function(e){
-	var key = e.which || e.keyCode;
-    if (key === 13) {     
-	todoitem1.setAttribute("value",add.value)
-	todoitem6.classList.toggle("todoitem6");
-	;
-}
+
+ addbtn.addEventListener("click", function(){
+    var re = /A-Za-z/
+        if (addtextbox.value == "" || addtextbox.value ==! re || addtextbox.value == null)
+            {  alert("please input a todo item before submitting"); }
+            else
+	           var addnewtodo= document.createElement("input");
+                document.querySelector("#left").appendChild(addnewtodo).setAttribute("class", "form-control");
+                document.querySelector("#left").appendChild(addnewtodo).setAttribute("value", addtextbox.value);
+                document.querySelector("#left").appendChild(addnewtodo).setAttribute("disabled", "");
+                addtextbox.value = ""; 
+                
 });
 
 
-
+addtextbox.addEventListener("keypress", function(e){
+	var key = e.which || e.keyCode;
+    if (key === 13) {  
+        var re = /A-Za-z/
+        if (addtextbox.value == "" || addtextbox.value ==! re || addtextbox.value == null){   
+              alert("Please enter a valid todo item"); 
+        }
+        else
+    var addnewtodo= document.createElement("input");
+    document.querySelector("#left").appendChild(addnewtodo).setAttribute("class", "form-control");
+    document.querySelector("#left").appendChild(addnewtodo).setAttribute("value", addtextbox.value);
+    document.querySelector("#left").appendChild(addnewtodo).setAttribute("disabled", "");
+    addtextbox.value = ""; 
+     }
+});
 
 
 
@@ -23,10 +40,17 @@ addtextbox.addEventListener("keypress", function(e){
 //				[addtextbox] when [enter key] is pressed
 
 
+// SAMPLE CODE OF CREATE EVENT
+// function addItem() {
+//     var addline = document.createElement("div");
+//     var t = document.createTextNode("asdf");
+//     addline.appendChild(t);
+//     document.body.appendChild(addline);
+// }
 
-function addItem() {
-    var btn = document.createElement("div");
-    var t = document.createTextNode("asdf");
-    btn.appendChild(t);
-    document.body.appendChild(btn);
-}
+// for (i=0; asdf < asdf.length; i++)
+// {
+
+// }
+
+// todoitem1.setAttribute("value",add.value)
