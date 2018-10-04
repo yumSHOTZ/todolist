978ecd24523b209d2e20cd0feeb7d239bd3d4142
michael.tablante <mtablate@venn.bio>
2018-10-04 16:18:07 +0800

    added edit button functionality

--- dashboard/js/TodoList.js
+++ dashboard/js/TodoList.js
@@ -1,6 +1,8 @@
 var addtextbox = document.querySelector("#addtodo");
 var left = document.querySelector("#left");
 var addbtn = document.querySelector("#addbtn");
+var counter = 0;
+
 
 
  addbtn.addEventListener("click", function(){
@@ -8,47 +10,84 @@ var addbtn = document.querySelector("#addbtn");
         if (addtextbox.value == "" || addtextbox.value ==! re || addtextbox.value == null)
             {  alert("please input a todo item before submitting"); }
             else
+            {
+// // for (counter=0; counter < specialbutton.length; counter++)
+// {
+                counter++;
 	            var addnewtodo= document.createElement("input");
                 var addbutton= document.createElement("input");
 
-                document.querySelector("#left").appendChild(addnewtodo).setAttribute("class", "form-control");
-                document.querySelector("#left").appendChild(addnewtodo).setAttribute("value", addtextbox.value);
-                document.querySelector("#left").appendChild(addnewtodo).setAttribute("disabled", "");
-                document.querySelector("#left").appendChild(addnewtodo).setAttribute("id", "specialtextbox");
+                    // ADD
+                left.appendChild(addnewtodo).setAttribute("class", "form-control styleTextbox");
+                left.appendChild(addnewtodo).setAttribute("value", addtextbox.value);
+                left.appendChild(addnewtodo).setAttribute("disabled", "");
+                left.appendChild(addnewtodo).setAttribute("id", "specialtextbox" + counter);
                 addtextbox.value = "";
-                document.querySelector("#left").appendChild(addbutton).setAttribute("class", "btn btn-default")
-                document.querySelector("#left").appendChild(addbutton).setAttribute("type", "button");
-                document.querySelector("#left").appendChild(addbutton).setAttribute("id", "specialbutton");
-                document.querySelector("#left").appendChild(addbutton).setAttribute("value", "EDIT");
+                    // EDIT
+                left.appendChild(addbutton).setAttribute("class", "btn btn-default styleButton")
+                left.appendChild(addbutton).setAttribute("type", "button");
+                left.appendChild(addbutton).setAttribute("id", "specialbutton" + counter);
+                left.appendChild(addbutton).setAttribute("value", "EDIT");
 
-
+                    var specialtextbox = document.querySelector("#specialtextbox" + counter);
+                    var specialbutton = document.querySelector("#specialbutton" + counter);
+                    specialbutton.addEventListener("click", function(){
+              
+                    if(specialbutton.value == "EDIT"){
+                        specialtextbox.removeAttribute("disabled");
+                        specialtextbox.setAttribute("class", "form-control justAclass styleTextbox");
+                        specialbutton.setAttribute("value", "OK");
+                    }
+                    else{
+                        specialtextbox.setAttribute("disabled", "");
+                        specialtextbox.setAttribute("class", "form-control styleTextbox");
+                        specialbutton.setAttribute("value", "EDIT");
+                        specialbutton.setAttribute("class", "btn btn-default styleButton")
+                    }             
+                });
+                }
+                
+                
 });
 
 
-addtextbox.addEventListener("keypress", function(e){
-	var key = e.which || e.keyCode;
-    if (key === 13) {
-        var re = /A-Za-z/
-        if (addtextbox.value == "" || addtextbox.value ==! re || addtextbox.value == null){
-              alert("Please enter a valid todo item");
-        }
-        else
-    var addnewtodo= document.createElement("input");
-    var addbutton= document.createElement("input");
-    document.querySelector("#left").appendChild(addnewtodo).setAttribute("class", "form-control");
-    document.querySelector("#left").appendChild(addnewtodo).setAttribute("value", addtextbox.value);
-    document.querySelector("#left").appendChild(addnewtodo).setAttribute("disabled", "");
-    addtextbox.value = "";
-    document.querySelector("#left").appendChild(addbutton).setAttribute("class", "btn btn-default")
-    document.querySelector("#left").appendChild(addbutton).setAttribute("type", "button");
-    document.querySelector("#left").appendChild(addbutton).setAttribute("id", "specialbutton");
-    document.querySelector("#left").appendChild(addbutton).setAttribute("value", "EDIT");
-     }
-});
 
 
 
+// addtextbox.addEventListener("keypress", function(e){
+// 	var key = e.which || e.keyCode;
+//     if (key === 13) {
+//         var re = /A-Za-z/
+//         if (addtextbox.value == "" || addtextbox.value ==! re || addtextbox.value == null){
+//               alert("Please enter a valid todo item");
+//         }
+//         else
+//                 var addnewtodo= document.createElement("input");
+//                 var addbutton= document.createElement("input");
 
+
+//                 left.appendChild(addnewtodo).setAttribute("class", "form-control");
+//                 left.appendChild(addnewtodo).setAttribute("value", addtextbox.value);
+//                 left.appendChild(addnewtodo).setAttribute("disabled", "");
+//                 left.appendChild(addnewtodo).setAttribute("id", "specialtextbox");
+//                 addtextbox.value = "";
+//                 left.appendChild(addbutton).setAttribute("class", "btn btn-default")
+//                 left.appendChild(addbutton).setAttribute("type", "button");
+//                 left.appendChild(addbutton).setAttribute("id", "specialbutton");
+//                 left.appendChild(addbutton).setAttribute("value", "EDIT");
+
+//                     var specialtextbox = document.querySelector("#specialtextbox");
+//                     var specialbutton = document.querySelector("#specialbutton");
+//                     specialbutton.addEventListener("click", function(){
+//                     specialtextbox.removeAttribute("disabled");
+//                 });
+
+//      }
+// });
+
+
+
+                
 
 
 // todo -     	ADD a toggle function that disables and enables an <input type text> inside #left
@@ -57,9 +96,10 @@ addtextbox.addEventListener("keypress", function(e){
 
 
 
-function addItem() {
-    var btn = document.createElement("div");
-    var t = document.createTextNode("asdf");
-    btn.appendChild(t);
-    document.body.appendChild(btn);
-}
+// function addItem() {
+//     var btn = document.createElement("div");
+//     var t = document.createTextNode("asdf");
+//     btn.appendChild(t);
+//     document.body.appendChild(btn);
+// }
+
