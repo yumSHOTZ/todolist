3e876f667ed5b99b2ed8fe3480a3fc5d8aca4f12
jimbs <balmorijimbo@yahoo.com>
2018-10-08 17:22:33 +0800

    Add class noContent if nothing to show in dashboard

--- dashboard/js/date.js
+++ dashboard/js/date.js
@@ -1,23 +1,21 @@
+document.getElementById('body').onload = function() { currentDate() };
 
+function currentDate() {
+    var d = new Date();
 
-function currentDate(){
-	var d = new Date();
+    var today = d.getDay();
+    var month = d.getMonth();
+    var year = d.getFullYear();
+    var date = d.getDate();
 
-	var today = d.getDay();
-	var month = d.getMonth();
-	var year = d.getFullYear();
-	var date = d.getDate();
+    var days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+    var months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
 
-	var days = ['Sunday','Monday','Tuesday','Wednesday','Thursday','Friday','Saturday'];
-	var months = ['January','February','March','April','May','June','July','August','September','October','November','December'];
+    var n_month = months[month];
+    var n_day = days[today];
 
-	var n_month = months[month];
-	var n_day = days[today];
+    var completeDate = n_day + ", " + n_month + " " + date + " " + year;
 
-	var completeDate = n_day + ", " + n_month + " " + date + " " + year;
+    document.getElementById('date').innerHTML = completeDate;
 
-	document.getElementById('date').innerHTML = completeDate;
-
-
-document.getElementById('body').onload = function(){currentDate()};
 }
